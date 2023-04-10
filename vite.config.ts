6256b43d11812ac2c263e012e7973dbea80f4534
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2023-04-10 15:46:04 -0300

    Remove PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,7 @@
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-      },
-      includeAssets: ["favicon.ico", "fiap.png"],
-      manifest: {
-        name: "My Awesome App",
-        short_name: "MyApp",
-        description: "My Awesome App description",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "fiap.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "fiap.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
