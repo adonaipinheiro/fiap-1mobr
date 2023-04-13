bf0c4dbaa9add5d93229a5c722bc5b8aaa48855e
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2023-04-13 20:51:40 -0300

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "FIAP",
+        short_name: "FIAP",
+        description: "Educação que transforma",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "fiap_192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "fiap.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
